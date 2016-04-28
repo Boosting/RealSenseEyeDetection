66f597ed1e5628a187299c84eabeb634e40e241d
charlieyes <charlielyz@gmail.com>
2016-04-28 14:05:22 -0700

    eye like is working! pupil detection is good.

--- RealSenseEyeDetection/EyeDetector.cpp
+++ RealSenseEyeDetection/EyeDetector.cpp
@@ -3,7 +3,7 @@
 
 EyeDetector::EyeDetector()
 {
-	if (!cascade.load(CASCADE_FACE_XML_LOCATION) || !nestedCascade.load(CASCADE_EYE_XML_LOCATION))
+	if (!cascade.load(casdace_face_xml_location) || !nestedCascade.load(casdace_eye_xml_location))
 	{
 		cout << "ERROR: Could not load Face / Eye classifier cascade" << endl;
 	}
@@ -63,42 +63,26 @@ void EyeDetector::CascadeDetection(Mat& inputImg) {
 
 void EyeDetector::PupilDetection(Mat& inputImg){
 	if (rawEyes.empty()) return;
-	bool findPupil = false;
 
-	for (vector<Rect>::const_iterator eyeItr = rawEyes.begin(); eyeItr != rawEyes.end(); ++eyeItr) {
-		findPupil = false;
-		Mat grayROI(inputImg, (*eyeItr));
+	size_t numFaces = rawFaces.size();
+	size_t numEyes = rawEyes.size();
 
-		// Convert to binary image by thresholding it
-		threshold(grayROI, grayROI, 220, 255, THRESH_BINARY);
+	vector<Rect>::const_iterator faceItr = rawFaces.begin();
+	vector<Rect>::const_iterator eyeItr = rawEyes.begin();
 
-		// Find all contours
-		vector<vector<Point>> contours;
-		findContours(grayROI.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+	for (int i = 0; i < numFaces; ++i) {
+		cout << "face loc : " << (*faceItr).x << '\t' << (*faceItr).y << endl;
+		for (int j = 0; j < 2; ++j) {
+			cout << "eye loc : " << (*eyeItr).x << '\t' << (*eyeItr).y << endl;
 
-		// Fill holes in each contour
-		drawContours(grayROI, contours, -1, CV_RGB(255, 255, 255), -1);
+			Rect eyeROI = Rect((*faceItr).x + (*eyeItr).x, (*faceItr).y + (*eyeItr).y, (*eyeItr).width, (*eyeItr).height);
+			Point pupil = findEyeCenter(inputImg, eyeROI);
+			rawPupils.push_back(pupil);
+			++eyeItr;
 
-		for (int i = 0; i < contours.size(); i++)
-		{
-			double area = contourArea(contours[i]);
-			Rect rect = boundingRect(contours[i]);
-			int radius = rect.width / 2;
-
-			// If contour is big enough and has round shape
-			// Then it is the pupil
-			if (area >= 30 &&
-				std::abs(1 - ((double)rect.width / (double)rect.height)) <= 0.2 &&
-				std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)
-			{
-				rawPupils.push_back(Point(rect.x + radius, rect.y + radius));
-				findPupil = true;
-				break;
-			}
+			cout << "pupil loc : " << pupil.x << '\t' << pupil.y << endl;
 		}
-
-		if (findPupil == false)
-			rawPupils.push_back(Point((*eyeItr).x + (*eyeItr).width / 2, (*eyeItr).y + (*eyeItr).height / 2));
+		++faceItr;
 	}
 }
 
@@ -115,7 +99,7 @@ void EyeDetector::ImageProcessAndDetect(Mat& colorImg, Mat& depth_to_color_img, 
 	for (int w = 0; w < original_Image_size.width; ++w) {
 		for (int h = 0; h < original_Image_size.height; ++h) {
 			double val = (double)depth_to_color_img.at<uint16_t>(h, w);
-			if (val < one_meter_d * CLOSEST_DEPTH_DISTANCE || val > one_meter_d * FARTHEST_DEPTH_DISTANCE) continue;
+			if (val < one_meter_d * closest_depth_distance || val > one_meter_d * farthest_depth_distance) continue;
 			if (h < roi_lt_point.y) roi_lt_point.y = h;
 			else if (h > roi_rb_point.y) roi_rb_point.y = h;
 			if (w < roi_lt_point.x) roi_lt_point.x = w;
@@ -126,14 +110,14 @@ void EyeDetector::ImageProcessAndDetect(Mat& colorImg, Mat& depth_to_color_img, 
 	// If nothing appears, return
 	if (roi_rb_point.x <= roi_lt_point.x || roi_rb_point.y <= roi_lt_point.y) return;
 
-	cvtColor(colorImg, gray_Image, COLOR_BGR2GRAY);
+	cvtColor(colorImg, gray_Image, CV_BGR2GRAY);
 
 	// Show refined ROI, use this ROI to detect faces
 	rectangle(colorImg, roi_lt_point, roi_rb_point, CV_RGB(255, 0, 0), 1, 8, 0);
 	roi_Image = Mat(gray_Image, Rect(roi_lt_point.x, roi_lt_point.y, roi_rb_point.x - roi_lt_point.x + 1, roi_rb_point.y - roi_lt_point.y + 1));
 	rotated_Image = Mat(roi_Image.size(), CV_8UC1);
 
-	for (int i = 0; i < 5; ++i) {
+	for (int i = 0; i < 3 ; ++i) {
 		// Rotate image in different directions, once get the eyes' coordinates, rotate the positions back to original image
 		double rotate = rorates[i];
 		Point2d pt(roi_Image.cols / 2., roi_Image.rows / 2.);
@@ -144,6 +128,7 @@ void EyeDetector::ImageProcessAndDetect(Mat& colorImg, Mat& depth_to_color_img, 
 		// Detect face and eyes, save information
 		CascadeDetection(rotated_Image);
 		PupilDetection(rotated_Image);
+
 		size_t numFaces = rawFaces.size();
 		if (numFaces == 0) continue;
 		if (numFaces * 2 != rawEyes.size()) continue;
@@ -158,7 +143,6 @@ void EyeDetector::ImageProcessAndDetect(Mat& colorImg, Mat& depth_to_color_img, 
 		vector<Point>::const_iterator pupilItr = rawPupils.begin();
 
 		for (int j = 0; j < numFaces; ++j) {
-
 			Rect tmpFace = Rect((*faceItr).x + roi_lt_point.x, (*faceItr).y + roi_lt_point.y, (*faceItr).width, (*faceItr).height);
 			if (IsFaceOverlap(tmpFace)) {
 				++faceItr;
@@ -177,6 +161,11 @@ void EyeDetector::ImageProcessAndDetect(Mat& colorImg, Mat& depth_to_color_img, 
 				resultPupils.push_back(Point((*pupilItr).x + roi_lt_point.x, (*pupilItr).y + roi_lt_point.y));
 				++pupilItr;
 			}
+
+			// Fill detected faces with black - to avoid the waste time of detecting the same face
+			// Mat blackMask = Mat(tmpFace.width, tmpFace.height, CV_8UC1, 0.0);
+			// Mat aux = roi_Image.colRange(tmpFace.x, tmpFace.x + tmpFace.width).rowRange(tmpFace.y, tmpFace.y + tmpFace.height);
+			// blackMask.copyTo(aux);
 		}
 
 		rawFaces.clear();
@@ -256,18 +245,20 @@ void EyeDetector::rotateBackRawInfo(Mat& rbMat) {
 		for (int j = 0; j < 2; ++j) {
 			Point eyeLT(faceLT.x + (*eyeItr).x, faceLT.y + (*eyeItr).y);
 			Point eyeRB(eyeLT.x + (*eyeItr).width, eyeLT.y + (*eyeItr).height);
+
+			Point pupil(eyeLT.x + (*pupilItr).x, eyeLT.y + (*pupilItr).y);
+			pupil = rotateBackPoints(pupil, rbMat);
+			(*pupilItr).x = pupil.x;
+			(*pupilItr).y = pupil.y;
+
 			eyeLT = rotateBackPoints(eyeLT, rbMat);
 			eyeRB = rotateBackPoints(eyeRB, rbMat);
 			(*eyeItr).x = eyeLT.x;
 			(*eyeItr).y = eyeLT.y;
 			(*eyeItr).width = eyeRB.x - eyeLT.x;
 			(*eyeItr).height = eyeRB.y - eyeLT.y;
-			++eyeItr;
 
-			Point pupil(faceLT.x + (*pupilItr).x, faceLT.y + (*pupilItr).y);
-			pupil = rotateBackPoints(pupil, rbMat);
-			(*pupilItr).x = pupil.x;
-			(*pupilItr).y = pupil.y;
+			++eyeItr;
 			++pupilItr;
 		}
 
@@ -298,4 +289,160 @@ const size_t EyeDetector::getEyesSize() {
 const Rect EyeDetector::getEyeLoc(int num) {
 	assert(num <= resultEyes.size());
 	return resultEyes[num];
+}
+
+//////////////////////// eyeLike functions ////////////////////////////
+
+// It returns eye center location offset, based on location of eyes.
+Point EyeDetector::findEyeCenter(Mat& face, Rect& eye) {
+	Mat eyeROIUnscaled = face(eye);
+	Mat eyeROI;
+
+	scaleToFastSize(eyeROIUnscaled, eyeROI);
+
+	//-- Find the gradient
+	cv::Mat gradientX = computeMatXGradient(eyeROI);
+	cv::Mat gradientY = computeMatXGradient(eyeROI.t()).t();
+
+	//-- Normalize and threshold the gradient
+	// compute all the magnitudes
+	cv::Mat mags = matrixMagnitude(gradientX, gradientY);
+	//compute the threshold
+	double gradientThresh = computeDynamicThreshold(mags, kGradientThreshold);
+	//double gradientThresh = kGradientThreshold;
+	//double gradientThresh = 0;
+	//normalize
+	for (int y = 0; y < eyeROI.rows; ++y) {
+		double *Xr = gradientX.ptr<double>(y), *Yr = gradientY.ptr<double>(y);
+		const double *Mr = mags.ptr<double>(y);
+		for (int x = 0; x < eyeROI.cols; ++x) {
+			double gX = Xr[x], gY = Yr[x];
+			double magnitude = Mr[x];
+			if (magnitude > gradientThresh) {
+				Xr[x] = gX / magnitude;
+				Yr[x] = gY / magnitude;
+			}
+			else {
+				Xr[x] = 0.0;
+				Yr[x] = 0.0;
+			}
+		}
+	}
+
+	//-- Create a blurred and inverted image for weighting
+	cv::Mat weight;
+	GaussianBlur(eyeROI, weight, cv::Size(kWeightBlurSize, kWeightBlurSize), 0, 0);
+	for (int y = 0; y < weight.rows; ++y) {
+		unsigned char *row = weight.ptr<unsigned char>(y);
+		for (int x = 0; x < weight.cols; ++x) {
+			row[x] = (255 - row[x]);
+		}
+	}
+	//imshow(debugWindow,weight);
+	//-- Run the algorithm!
+	cv::Mat outSum = cv::Mat::zeros(eyeROI.rows, eyeROI.cols, CV_64F);
+	// for each possible gradient location
+	// Note: these loops are reversed from the way the paper does them
+	// it evaluates every possible center for each gradient location instead of
+	// every possible gradient location for every center.
+	for (int y = 0; y < weight.rows; ++y) {
+		const double *Xr = gradientX.ptr<double>(y), *Yr = gradientY.ptr<double>(y);
+		for (int x = 0; x < weight.cols; ++x) {
+			double gX = Xr[x], gY = Yr[x];
+			if (gX == 0.0 && gY == 0.0) {
+				continue;
+			}
+			testPossibleCentersFormula(x, y, weight, gX, gY, outSum);
+		}
+	}
+	// scale all the values down, basically averaging them
+	double numGradients = (weight.rows*weight.cols);
+	cv::Mat out;
+	outSum.convertTo(out, CV_32F, 1.0 / numGradients);
+	//imshow("debugwindow",out);
+	//-- Find the maximum point
+	cv::Point maxP;
+	double maxVal;
+	cv::minMaxLoc(out, NULL, &maxVal, NULL, &maxP);
+
+	cv::Point result = unscalePoint(maxP, eye);
+	return result;
+}
+
+void EyeDetector::scaleToFastSize(const Mat &src, Mat &dst) {
+	resize(src, dst, Size(kFastEyeWidth, (((float)kFastEyeWidth) / src.cols) * src.rows));
+}
+
+Mat EyeDetector::computeMatXGradient(const Mat &mat) {
+	Mat out(mat.rows, mat.cols, CV_64F);
+
+	for (int y = 0; y < mat.rows; ++y) {
+		const uchar *Mr = mat.ptr<uchar>(y);
+		double *Or = out.ptr<double>(y);
+
+		Or[0] = Mr[1] - Mr[0];
+		for (int x = 1; x < mat.cols - 1; ++x) {
+			Or[x] = (Mr[x + 1] - Mr[x - 1]) / 2.0;
+		}
+		Or[mat.cols - 1] = Mr[mat.cols - 1] - Mr[mat.cols - 2];
+	}
+
+	return out;
+}
+
+cv::Mat EyeDetector::matrixMagnitude(const cv::Mat &matX, const cv::Mat &matY) {
+	cv::Mat mags(matX.rows, matX.cols, CV_64F);
+	for (int y = 0; y < matX.rows; ++y) {
+		const double *Xr = matX.ptr<double>(y), *Yr = matY.ptr<double>(y);
+		double *Mr = mags.ptr<double>(y);
+		for (int x = 0; x < matX.cols; ++x) {
+			double gX = Xr[x], gY = Yr[x];
+			double magnitude = sqrt((gX * gX) + (gY * gY));
+			Mr[x] = magnitude;
+		}
+	}
+	return mags;
+}
+
+double EyeDetector::computeDynamicThreshold(const cv::Mat &mat, double stdDevFactor) {
+	cv::Scalar stdMagnGrad, meanMagnGrad;
+	cv::meanStdDev(mat, meanMagnGrad, stdMagnGrad);
+	double stdDev = stdMagnGrad[0] / sqrt(mat.rows*mat.cols);
+	return stdDevFactor * stdDev + meanMagnGrad[0];
+}
+
+void EyeDetector::testPossibleCentersFormula(int x, int y, const cv::Mat &weight, double gx, double gy, cv::Mat &out) {
+	// for all possible centers
+	for (int cy = 0; cy < out.rows; ++cy) {
+		double *Or = out.ptr<double>(cy);
+		const unsigned char *Wr = weight.ptr<unsigned char>(cy);
+		for (int cx = 0; cx < out.cols; ++cx) {
+			if (x == cx && y == cy) {
+				continue;
+			}
+			// create a vector from the possible center to the gradient origin
+			double dx = x - cx;
+			double dy = y - cy;
+			// normalize d
+			double magnitude = sqrt((dx * dx) + (dy * dy));
+			dx = dx / magnitude;
+			dy = dy / magnitude;
+			double dotProduct = dx*gx + dy*gy;
+			dotProduct = std::max(0.0, dotProduct);
+			// square and multiply by the weight
+			if (kEnableWeight) {
+				Or[cx] += dotProduct * dotProduct * (Wr[cx] / kWeightDivisor);
+			}
+			else {
+				Or[cx] += dotProduct * dotProduct;
+			}
+		}
+	}
+}
+
+cv::Point EyeDetector::unscalePoint(cv::Point p, cv::Rect origSize) {
+	float ratio = (((float)kFastEyeWidth) / origSize.width);
+	int x = round(p.x / ratio);
+	int y = round(p.y / ratio);
+	return cv::Point(x, y);
 }
--- RealSenseEyeDetection/EyeDetector.h
+++ RealSenseEyeDetection/EyeDetector.h
@@ -4,8 +4,7 @@
 #include <opencv2/imgproc.hpp>		// circle is in here
 #include <opencv2/objdetect.hpp>	// CascadeClassifier is in here
 #include <opencv2/highgui.hpp>		// imshow is in here
-
-#include "Parameters.h"
+#include <queue>
 
 using namespace cv;
 using namespace std;
@@ -13,6 +12,16 @@ using namespace std;
 class EyeDetector
 {
 private:
+
+	/////////////// constants in eye detection //////////////////////
+
+	const string casdace_face_xml_location = "D:/Lib/OpenCV/build/etc/haarcascades/haarcascade_frontalface_default.xml";
+	const string casdace_eye_xml_location = "D:/Lib/OpenCV/build/etc/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
+
+	// Closest and Farthest face detection distance (in meters)
+	const float closest_depth_distance = 0.5;
+	const float farthest_depth_distance = 3.0;
+
 	const Scalar colors[8] = { CV_RGB(0,0,255),
 		CV_RGB(0,128,255),
 		CV_RGB(0,255,255),
@@ -21,14 +30,24 @@ private:
 		CV_RGB(255,255,0),
 		CV_RGB(255,0,0),
 		CV_RGB(255,0,255) };
-	const double rorates[5] = {0.0, 15.0, -15.0, 30.0, -30.0};
+	const double rorates[3] = {0.0, -20.0, 20.0};
 	const double scale = 1.0;
+
+	// Algorithm Parameters
+	const int kFastEyeWidth = 50;
+	const int kWeightBlurSize = 5;
+	const bool kEnableWeight = true;
+	const float kWeightDivisor = 1.0;
+	const double kGradientThreshold = 50.0;
+
 	CascadeClassifier cascade;
 	CascadeClassifier nestedCascade;
 
-	vector<Rect> rawFaces;
-	vector<Rect> rawEyes;
-	vector<Point> rawPupils;
+	vector<Rect> rawFaces;		// rawFaces: saving absolute locations
+	vector<Rect> rawEyes;		// rawEyes: before rotate back: saving location offset corresponding to faces
+								//          after rotate back: saving absolute locations
+	vector<Point> rawPupils;	// rawPupils: before rotate back: saving location offset corresponding to eyes
+								//            after rotate back: saving absolute locations
 	vector<Rect> resultFaces;	// Faces: saving absolute locations
 	vector<Rect> resultEyes;	// Eyes: saving absolute locations
 	vector<Point> resultPupils;	// Pupils: pupil center corresponding to eyes, absolute locations.
@@ -45,6 +64,15 @@ private:
 
 	Point rotateBackPoints(Point srcPoint, Mat& rbMat);
 	void rotateBackRawInfo(Mat& rbMat);
+
+	// eyeLike functions - proessing
+	Point findEyeCenter(Mat& face, Rect& eye);
+	void scaleToFastSize(const Mat &src, Mat &dst);
+	Mat computeMatXGradient(const Mat &mat);
+	cv::Mat matrixMagnitude(const cv::Mat &matX, const cv::Mat &matY);
+	double computeDynamicThreshold(const cv::Mat &mat, double stdDevFactor);
+	void EyeDetector::testPossibleCentersFormula(int x, int y, const cv::Mat &weight, double gx, double gy, cv::Mat &out);
+	cv::Point EyeDetector::unscalePoint(cv::Point p, cv::Rect origSize);
 
 public:
 	EyeDetector();
